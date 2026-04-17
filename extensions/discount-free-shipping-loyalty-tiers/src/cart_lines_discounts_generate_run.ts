62e968ac0b655790580692c878346b7bd910005f
David <da3909622@gmail.com>
2026-04-17 12:29:49 -0600

    Init Delivery discount

---
+++ extensions/discount-free-shipping-loyalty-tiers/src/cart_lines_discounts_generate_run.ts
@@ -0,0 +1,20 @@
+import {
+  DiscountClass,
+  OrderDiscountSelectionStrategy,
+  ProductDiscountSelectionStrategy,
+  CartInput,
+  CartLinesDiscountsGenerateRunResult,
+} from '../generated/api';
+
+
+export function cartLinesDiscountsGenerateRun(
+  input: CartInput,
+): CartLinesDiscountsGenerateRunResult {
+
+  const operations = [];
+
+
+  return {
+    operations: [],
+  };
+}
